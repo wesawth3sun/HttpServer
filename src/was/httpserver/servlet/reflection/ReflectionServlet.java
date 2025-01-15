6e513e26f1d77cd58f436e69eb28843f05601041
SUN <suunn001@gmail.com>
2025-01-15 16:53:39 +0900

    HTTP 서버 리플렉션 추가

---
+++ src/was/httpserver/servlet/reflection/ReflectionServlet.java
@@ -0,0 +1,49 @@
+package was.httpserver.servlet.reflection;
+
+import was.httpserver.HttpRequest;
+import was.httpserver.HttpResponse;
+import was.httpserver.HttpServlet;
+import was.httpserver.PageNotFoundException;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class ReflectionServlet implements HttpServlet {
+
+    //SiteController, SearchController 받을 용도
+    private final List<Object> controllers;
+
+    public ReflectionServlet(List<Object> controllers) {
+        this.controllers = controllers;
+    }
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) throws IOException {
+        // /site1, /site2, /search 등의 URI path 가 넘어옴
+        String path = request.getPath();
+
+        for (Object controller : controllers) {
+            //컨트롤러들에서 클래스 정보를 가져옴 -> 메소드 이름 가져오기 위해서
+            Class<?> aClass = controller.getClass();
+            //어떤 컨트롤러 클래스에 있는 모든 메서드를 다 가져옴
+            Method[] methods = aClass.getDeclaredMethods();
+            for (Method method : methods) {
+                //메서드 이름을 가져오기 = path랑 비교하기 위해서
+                String methodName = method.getName();
+                if (path.equals("/" + methodName)) {
+                    try {
+                        method.invoke(controller, request, response);
+                        return;
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException("Access to method denied", e);
+                    } catch (InvocationTargetException e) {
+                        throw new RuntimeException("Method invocation failed", e);
+                    }
+                }
+            }
+        }
+        throw new PageNotFoundException("request = " + path);
+    }
+}
--- src/was/v6/ServerMainV6.java
+++ src/was/v6/ServerMainV6.java
@@ -1,28 +1,32 @@
-package was.v5;
+package was.v6;
 
 import was.httpserver.HttpServer;
+import was.httpserver.HttpServlet;
 import was.httpserver.ServletManager;
 import was.httpserver.servlet.DiscardServlet;
-import was.v4.HttpServerV4;
+import was.httpserver.servlet.reflection.ReflectionServlet;
 import was.v5.servlet.HomeServlet;
 import was.v5.servlet.SearchServlet;
 import was.v5.servlet.Site1Servlet;
 import was.v5.servlet.Site2Servlet;
 
 import java.io.IOException;
+import java.util.List;
 
-public class ServerMainV5 {
+public class ServerMainV6 {
 
     private static final int PORT = 12345;
 
     public static void main(String[] args) throws IOException {
+
         //각 서블릿 클래스들 -> 하나의 클래스는 하나의 기능만 구현 가능한 단점
         //새로 서블릿이 추가되면 메인 클래스에서도 추가해 매핑해 주어야 하는 단점
+
+        HttpServlet reflectionServlet = new ReflectionServlet(List.of(new SiteControllerV6(), new SearchControllerV6()));
         ServletManager servletManager = new ServletManager();
+        servletManager.setDefaultServlet(reflectionServlet);
+
         servletManager.add("/", new HomeServlet());
-        servletManager.add("/site1", new Site1Servlet());
-        servletManager.add("/site2", new Site2Servlet());
-        servletManager.add("/search", new SearchServlet());
         servletManager.add("/favicon.ico", new DiscardServlet());
 
         HttpServer server = new HttpServer(PORT, servletManager);
