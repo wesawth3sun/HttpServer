393d83a3dd853a5ccc8297a4ca7007c5cac0f923
SUN <suunn001@gmail.com>
2025-01-16 17:14:58 +0900

    http 서버 최적화 구현

--- src/was/v8/ServerMainV8.java
+++ src/was/v8/ServerMainV8.java
@@ -4,6 +4,7 @@ import was.httpserver.HttpServer;
 import was.httpserver.HttpServlet;
 import was.httpserver.ServletManager;
 import was.httpserver.servlet.annotation.AnnotationServletV2;
+import was.httpserver.servlet.annotation.AnnotationServletV3;
 import was.v7.SearchControllerV7;
 
 import java.io.IOException;
@@ -17,7 +18,7 @@ public class ServerMainV8 {
 
         //이전과 같은데, 서블릿에 들어가는 변수만 동적 바인딩
 
-        HttpServlet annotationServlet = new AnnotationServletV2(List.of(new SiteControllerV8(), new SearchControllerV7()));
+        HttpServlet annotationServlet = new AnnotationServletV3(List.of(new SiteControllerV8(), new SearchControllerV7()));
         ServletManager servletManager = new ServletManager();
         servletManager.setDefaultServlet(annotationServlet);
 
